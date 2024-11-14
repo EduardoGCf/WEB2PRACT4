414d6c58b9d84cfa9604f806abaf76ed26c29aff
EduardoGCf <103554054+EduardoGCf@users.noreply.github.com>
2024-11-14 17:50:41 -0400

    A

--- front/spotify-front/src/pages/AdminPage.jsx
+++ front/spotify-front/src/pages/AdminPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab, Container } from 'react-bootstrap';
 import GeneroCrud from '../components/GeneroCrud';
 import AlbunCrud from '../components/AlbunCrud';
 import ArtistaCrud from '../components/ArtistaCrud';
@@ -9,25 +9,27 @@ function AdminPage() {
   const [key, setKey] = useState('genero');
 
   return (
-    <Tabs
-      id="admin-tabs"
-      activeKey={key}
-      onSelect={(k) => setKey(k)}
-      className="mb-3"
-    >
-      <Tab eventKey="genero" title="Géneros">
-        <GeneroCrud key={key} />
-      </Tab>
-      <Tab eventKey="artista" title="Artistas">
-        <ArtistaCrud key={key} />
-      </Tab>
-      <Tab eventKey="albun" title="Álbumes">
-        <AlbunCrud key={key} />
-      </Tab>
-      <Tab eventKey="cancion" title="Canciones">
-        <CancionCrud key={key} />
-      </Tab>
-    </Tabs>
+    <Container className="mb-5"> {/* Agregado el Container con clase mb-5 */}
+      <Tabs
+        id="admin-tabs"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Tab eventKey="genero" title="Géneros">
+          <GeneroCrud key={key} />
+        </Tab>
+        <Tab eventKey="artista" title="Artistas">
+          <ArtistaCrud key={key} />
+        </Tab>
+        <Tab eventKey="albun" title="Álbumes">
+          <AlbunCrud key={key} />
+        </Tab>
+        <Tab eventKey="cancion" title="Canciones">
+          <CancionCrud key={key} />
+        </Tab>
+      </Tabs>
+    </Container>
   );
 }
 
